9fd688642b9d747bdab8601903d4030053d48230
Pushpendra kumar sahu <153269194+Pushpendra2108@users.noreply.github.com>
2024-12-23 17:28:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function stringChop(str, size) {
-  // your code here
-	 let result = []; // Array to store the chunks
+  let result = []; // Array to store the chunks
 
-    for (let i = 0;  i<str.length;  i += length) {
-      result.push(str.slice(i, i + length)); // Slice the string from i to i+length
-    }
-    return result; // Return the array of chunks
-	
-}
+  for (let i = 0;  i < str.length;  i += size) {
+    result.push(str.slice(i, i + size)); // Slice the string from i to i+size
+  }
+  
+  return result; // Return the array of chunks
+} 
 
 // Do not change the code below
 const str = prompt("Enter String.");
